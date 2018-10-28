d82ac89e53c14d6cd972d0c372b426ccabf6e2a2
igorai <igorai@sapient.com>
2018-10-28 20:03:09 +0530

    initial commit by Indu

---
+++ src/main/java/com/indu/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,20 @@
+package com.indu.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+	
+	@RequestMapping("/whatisthis")
+    public String greeting() {
+        return "This is the EurekaServer !";
+    }
+}
